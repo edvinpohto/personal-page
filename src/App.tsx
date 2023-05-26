88f2972be78db8bd0c1bda8b62ab1fbffd55049c
Edvin Pohto <edvin.pohto@live.fi>
2023-05-26 17:52:19 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='content-container'>
+        <section>
+          <div className="content">
+            <h2>Coming Soon</h2>
+            <h2>Coming Soon</h2>
+          </div>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
